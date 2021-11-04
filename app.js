4bd516b5d068bc7a028eef43ea849ce24dd6f8af
Agustin <agualvila@gmail.com>
2021-11-03 21:21:45 -0300

    upload to s3 basic working

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 require('dotenv').config();
+
 const multer = require('multer'); //<- Libreria middleware para manejo facil de subida de archivos
-const upload = multer({ dest: 'uploads/'}); //<- carpeta para guardar temporalmente el archivo
+const upload = multer({ dest: 'uploads/' }); //<- carpeta para guardar temporalmente el archivo
+
+const s3FileService = require('./services/aws_s3');
 
 /*********************/
 // EXPRESS APP
@@ -26,11 +29,26 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.post('/upload', upload.single('image'), (req, res) => {
+app.post('/upload', upload.single('image'), async (req, res) => {
     const file = req.file; //<- info acerca del archivo
     const body = req.body; //<- body normal
-    console.log(file);
-    console.log(body);
+    //console.log(file);
+    //console.log(body);
+
+    const result = await s3FileService.uploadFile(file); //<- consume servicio s3
+
+    console.log(result); //<- result contiene data del archivo subido
+    /**
+     * ejemplo result:
+     {
+        ETag: '"9da280267af1b0f44145631ca80c7a6b"',
+        Location: 'https://cohorte-septiembre-91ddd87b.s3.amazonaws.com/342eeeb8c123597372068ccd4c13e656',
+        key: '342eeeb8c123597372068ccd4c13e656',
+        Key: '342eeeb8c123597372068ccd4c13e656',
+        Bucket: 'cohorte-septiembre-91ddd87b'
+    }
+     */
+    
     res.send('ok');
 });
 
--- services/aws_s3.js
+++ services/aws_s3.js
@@ -1,9 +1,33 @@
+// Import fs
+const fs = require('fs');
+
 // Import AWS SDK
 const AWS = require('aws-sdk');
+
 // Set the Region 
 AWS.config.update({ region: process.env.AWS_REGION });
+
+//Bucket
+const bucketName = process.env.AWS_BUCKET
+
 // Create S3 instance
 const s3 = new AWS.S3();
-//Bucket
-const bucket = process.env.AWS_BUCKET;
 
+
+// Upload file
+const uploadFile = (file) => {
+    const fileStream = fs.createReadStream(file.path)
+
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename
+    }
+
+    return s3.upload(uploadParams).promise();
+}
+
+// exports.uploadFile = uploadFile;
+module.exports = {
+    uploadFile
+}
